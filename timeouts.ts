a7686cbc605f4b95ba35fc3aee68567909e8190e
komevi01 <vinod.komeershetty.consultant@nielsen.com>
2020-09-23 18:12:05 +0530

    gt

--- timeouts.ts
+++
@@ -1,2 +0,0 @@
-var { setDefaultTimeout } = require('cucumber');  // Set Timeout for Cucumber to avoid - Error: function timed out, ensure the promise resolves within 5000 milliseconds 
-setDefaultTimeout(100 * 1000);
